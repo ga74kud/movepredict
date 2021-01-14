b83e0ae7208b2814292a9580d683002127992ba7
ga74kud <michaelhartmann10@googlemail.com>
2021-01-14 11:44:30 +0100

    init files
    Signed-off-by: ga74kud <michaelhartmann10@googlemail.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='movepredict',
+      version='0.1',
+      description='Movement prediction',
+      url='https://github.com/ga74kud/reachab',
+      author='Michael Hartmann',
+      author_email='michael.hartmann@v2c2.at',
+      license='GNU GENERAL PUBLIC LICENSE',
+      packages=['reachab'],
+      zip_safe=False)
